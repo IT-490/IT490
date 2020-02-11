7177f9dc48e16c1159441d29af6fa61dc4cc9fa3
UnknownEntity <jacobszepsy@gmail.com>
2020-02-11 17:48:21 +0000

    added some validation to login.php

--- frontend/login.php
+++ frontend/login.php
@@ -6,6 +6,24 @@ if(mysqli_connect_error()){
 	print('Failled to connect to database.' + mysqli_connect_error);
 	exit();
 }
+$name = sanatize($db, $_REQUEST['name']);
+$pass = sanatize($db, $_REQUEST['pass']);
+$errFlag = 0;
+
+if(empty($name)){
+	$errFlag = 1;
+	$nameError = 1;
+}
+if(empty($pass)){
+	$errFlag = 1;
+	$nameError = 1;
+}
+
+if($errFlag == 1){
+	//return a json object with error information
+}else{
+	sendRabbit("array of data here", "database")
+}
 
 
 ?>
