f09c9258b67df5cbc46952a7c7b8a7953a5ab241
UnknownEntity <jacobszepsy@gmail.com>
2020-02-17 06:05:12 +0000

    renamed some files. copied ini file into backend directory. added code to login and register scripts to start user session on successful attempt. further fleshed out php script on database server to include the sanatize function and also implemented rabbitMQ server code

---
+++ backend/rabbit.php
@@ -0,0 +1,43 @@
+<?php
+//Include databse connection info 
+
+function proccess($recieved){
+echo "received request".PHP_EOL;
+var_dump($request);	
+if($recieved['type'] == 'login'){
+	$sql = "Select username, password FROM users WHERE username = '$user'";
+	$result = mysqli_query($sql);
+	if(mysqli_num_rows($result) == 0){
+		return 1;
+	}
+	else{
+		while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+			if($pass == $row['password']){
+				return 0;
+			}
+			else{
+				return 2;
+			}	
+		}
+	}
+}else if($recieved['type'] == 'register'){
+	$s="select * from it490 where user = '$user' and pass = '$pass'";
+	$t="select top 1 id from users order by id desc";
+	$id = $t +1;
+	if($s != empty){
+		return 1;
+	}
+	else{
+		insert into users (id, username, password, firstname, lastname) values ($id, $user, $pass, $fname, $lname);
+	}
+}else if($recieved['type'] == 'sanatize'){
+	return mysqli_real_escape_string($db, $recieved['data']);
+}else if($recieved['type'] == 'log'){
+	//code to recieved logs from other systems
+}else if($recieved['type'] == 'update'){
+	//code to update database from data api
+}
+}
+$server = new rabbitMQServer("testRabbitMQ.ini", "testServer");
+$server->process_request('proccess');
+?>
