16a2bd4ff4e26d1926927e9f4fc35a5c0ddeb314
Bed3150n <benedelson9@gmail.com>
2020-02-18 17:51:21 +0000

    added the santize function to the backend function

--- backend/rabbit.php
+++ backend/rabbit.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 $db = mysqli_connect($hostname,$username,$password,$project);
 if(mysqli_connect_error()){
@@ -31,16 +32,18 @@ if($recieved['type'] == 'login'){
 	}
 }else if($recieved['type'] == 'register'){
 	$s="select * from it490 where user = '$user'";
-	$t="select top 1 id from users order by id desc";
-	$id = $t +1;
-	if($s != empty){
+	$result = mysqli_query($db,$s);
+	if(mysqli_num_rows($result) != 0){
 		return 1;
 	}
 	else{
-		insert into users (id, username, password, firstname, lastname) values ($id, $user, $pass, $fname, $lname);
+		
+		$sql="insert into users ('id', 'username', 'password', 'firstname', 'lastname') values ($id, $user, $pass, $fname, $lname)";
+		mysqli_query($db,$sql);
 		return 0;
 	}
 }else if($recieved['type'] == 'sanatize'){
+	return mysqli_real_escape_string($db, $recieved['data']);
 	
 }else if($recieved['type'] == 'log'){
 
