668b06b96049d3f2403e1de96fad89da398e392a
UnknownEntity <JacobSzepsy@gmail.com>
2020-02-20 15:10:43 -0500

    fixed variable name issue

--- frontend/functions.php
+++ frontend/functions.php
@@ -4,7 +4,7 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 	function sendRabbit($data){
-		$client = new rabbitMQClient("rabbitMQ.ini","testserver");
+		$client = new rabbitMQClient("rabbitMQ.ini","database");
 		$response = $client->send_request($data);
 		return $response;	
 	}
