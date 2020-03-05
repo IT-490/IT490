d272072eb69dac822c55f0943d041292e7445271
Bed3150n <benedelson9@gmail.com>
2020-03-05 16:06:14 +0000

    changed the mailer script to use swift rather than the built in php mail

--- frontend/email.php
+++ frontend/email.php
@@ -2,6 +2,15 @@
 include ('functions.php');
 $message = sendRabit(array('type'=> 'getSchedule','data'=>$user));
 $email - sendRabbit('type'=> 'email', 'data'=>$user));
-mail($email, "show schedule", $message );
-
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465))
+	->setUsersname('njitvemailer@gmail.com')
+	->setPassword('^0*1v4N0Zp')
+	;
+$mailer = new Swift_Mailer($transport);
+$body = (new Swift_Message('Subject'))
+	->setFrom('NJITV')
+	->setTo($email)
+	->setBody($message)
+	;
+$result= $mailer->send($body);
 ?>
