362c151ae86959de4023d1532560512817c2a201
UnknownEntity <jacobszepsy@gmail.com>
2020-03-10 05:30:32 +0000

    fixed path issue with errorlog listeners on rabbit and dataset servers

--- dataset/errorLog.php
+++ dataset/errorLog.php
@@ -1,7 +1,7 @@
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
+require_once('../path.inc');
+require_once('../get_host_info.inc');
+require_once('../rabbitMQLib.inc');
 function process($error){
 //	if(isset($error['type'])){
 //		return null;
