ac9b24fe8f32f270575014f2de685036732fc4ea
Bed3150n <benedelson9@gmail.com>
2020-05-08 01:08:50 +0000

    added the system to add changed code to the production server

--- deployment/deployment.php
+++ deployment/deployment.php
@@ -1,16 +1,32 @@
 <?php
-
+include ('../backend/account.php');
 echo $argv[1];
-$conn = new mysqli("localhost", "root", "delta523", "it490db");
-if($conn->connect_error){
-	die("Connection Failed" . $conn->connect_error);
+$db = mysqli_connect($hostname, $username, $password, $project);
+if(mysqli_connect_error()){
+	echo "Failed to connect to mysql:" .mysqli_connect_error();
+if($argv[1] == "pull"){
+	$data = sendRabbit($argv[3], 'development', $argv[2], rabbitMQ.ini);
+	$sql ="insert into versions (system, version, contents) values ('{$argv[2]}', '$argv[3]}' '$data')";
+	mysqli_query($db,$sql);
+	sendRabbit($data, 'production', $argv[2], rabbitMQ.ini);
 }
-
-$sql1 = "SELECT isBad FROM versions";
-if($sql1 == true){
-
+elseif($argv[1] == "set"){
+	$sql ="select from versions where system = $argv[2] and version = $argv[3]";
+	$result = mysqli_query($db, $sql);
+	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		if($row['isCurrent'] == 1){
+			$sql = "SELECT version, contents FROM versions WHERE system = $argv[2] AND isBad = 0 ORDER BY version DESC LIMIT 1";
+			$result = mysqli_query($db, $sql)
+			while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+				sendRabbit($row['contents'], 'production', $argv[2], rabbitMQ.ini);
+				$sql = "update versions SET isCurrent = 1 where version = '{$row['version']}'";
+				mysqli_query($db,$sql);
+   }
+  }
+ }
 }
-$sql= "INSERT INTO versions (system, version, isBad, contents) VALUES ()";
-
+else{
+	echo "error Command  $argv[1] not found";
+}
 
 ?>
